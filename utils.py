5c8af1e98fb1612b0dd9a8939b2e2b469fd331d8
Kaushal Grover <grover.kaushal27@gmail.com>
2023-04-30 00:00:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,12 +3,13 @@ import pandas as pd
 from scipy.stats import kstest
 import os
 import matplotlib.pyplot as plt
-from scipy.stats import mstats
+import MLR_function
+import matlab
 
 
 
 def EMT76GS(log2Exp,input_gseid):
-    print("Calculating EMT Score ....")
+    print("Calculating 76GS Score ....")
 
     EMTSignature = pd.read_excel("./Gene_signatures/76GS/EMT_signature_76GS.xlsx", header=0)
     EMTSignature = EMTSignature.dropna(subset=['Affymetrix Probe'])
@@ -111,47 +112,52 @@ def KSScore(expMat,input_gseid):
 
     return finalScore
 
-
-
-def plot_76GSvsKS(result_76GS, result_KS,input_gseid):
-    plt.scatter(result_76GS['GS76'],result_KS['KS'], marker='*', color='black')
-    plt.xlabel('EMT Score (76GS)')
-    plt.ylabel('EMT Score (KS)')
-    outfile = './graphs_generated/' + input_gseid 
-    if not os.path.exists(outfile):
-        os.makedirs(outfile)
-    plt.savefig(outfile +'/76GSvsKS.png')
-    plt.close()
-
     
-    
-    
+
 
 def MLR3(A1, A2, A3, A4):
-    a, b = A4.shape
-    if A4.shape[0] == A2.shape[0]:
-        A5 = mstats.mnrval(A1[:,0], A4.T, method='ordinal')
+    my_MLR_function = MLR_function.initialize()
+
+    b,a = A4.shape
+    if a == A2.shape[0]:
+        A1_linear_array = np.concatenate(A1.values.T)
+        A4_linear_array = np.concatenate(A4.values.T)
+        A1In = matlab.double(A1_linear_array, size=(4, 1))
+        A4In = matlab.double(A4_linear_array, size=(b, a))
+        A5 = my_MLR_function.MLR_function(A1In, A4In)
+        # A5 = mstats.mnrval(A1[:,0], A4.T, method='ordinal')
+        A5 = pd.DataFrame(A5)
         A6 = np.zeros((b,1))
         for j in range(b):
-            if np.array_equal(A5[j,:], np.nan*np.ones((1,A1.shape[0]-1))):
+            if np.array_equal(A5.iloc[j,:], np.nan*np.ones((1,A1.shape[0]-1))):
                 A6[j,0] = np.nan
             else:
-                A6[j,0] = np.argmax(A5[j,:]) + 1
+                A6[j,0] = np.argmax(A5.iloc[j,:]) +1
     else:
         index = []
         for j in range(A2.shape[0]):
             temp = np.where(A3 == np.array(A2.iloc[j,0]))
             index.append(temp[0][0])
-        A7 = np.zeros((A2.shape[0],b))
+        A7 = np.zeros((b,A2.shape[0]))
         for j in range(A2.shape[0]):
-            A7[j,:] = A4.iloc[index[j],:]
-        A5 = mnrval(A1.iloc[:,0], A7.T, method='ordinal')
+            A7[:,j] = A4.iloc[:,index[j]]
+        
+        temp1,temp2 = A7.shape
+        A1_linear_array = np.concatenate(A1.values.T)
+        A7_linear_array = np.concatenate(A7.values.T)
+        A1In = matlab.double(A1_linear_array, size=(4, 1))
+        A7In = matlab.double(A7_linear_array, size=(temp1,temp2))
+        A5 = my_MLR_function.MLR_function(A1In, A7In)
+        # A5 = mnrval(A1.iloc[:,0], A7.T, method='ordinal')
+        A5 = pd.DataFrame(A5)
         A6 = np.zeros((b,1))
         for j in range(b):
-            if np.array_equal(A5[j,:], np.nan*np.ones((1,A1.shape[0]-1))):
+            if np.array_equal(A5.iloc[j,:], np.nan*np.ones((1,A1.shape[0]-1))):
                 A6[j,0] = np.nan
             else:
-                A6[j,0] = np.argmax(A5[j,:]) + 1
+                A6[j,0] = np.argmax(A5.iloc[j,:]) + 1
+
+    my_MLR_function.terminate()
     return A5, A6
 
 
@@ -180,31 +186,39 @@ def MLRScore(geneExpMat,input_gseid,annot_table):
     geneExpMat.to_csv(outfile + '_MLR_exp_levels.csv', index=False) 
 
 
+    ## Now the following code takes the NCI60 dataset and takes the mean of probes per gene and selects only the genes MLR genes.
+
     DataNCI60 = pd.read_csv('./default_data/DataNCI60.txt', header=None, delim_whitespace=True)
-    
-    
-    # col = np.arange(0,DataNCI60.shape[1],1)
-    # DataNCI60 = pd.DataFrame(columns=col)
     DataNCI60[0] = DataNCI60[0].str.strip("'")
-    DataNCI60 = annot_table[['Gene Symbol','ID']].merge(DataNCI60, left_on='ID', right_on=0)
-    DataNCI60 = DataNCI60.drop(columns = ['ID'])
-
-
-    # replace nan and none values with 0.
     DataNCI60 = pd.DataFrame(DataNCI60)
-    DataNCI60.fillna(0, inplace=True)
-
-    # taking mean expression of all probes per gene.
-    DataNCI60 = DataNCI60.groupby('Gene Symbol').mean(numeric_only=True)
-
-    # merging MLR_genes with DataNCI60
-    DataNCI60 = geneList.merge(DataNCI60, left_on='MLR_genes', right_on='Gene Symbol')
-
-    NCI_data = DataNCI60.iloc[:,1:]
-    data = geneExpMat.iloc[0:25,1:]
 
 
+
+    # DataNCI60 = annot_table[['Gene Symbol','ID']].merge(DataNCI60, left_on='ID', right_on=0)
+    # DataNCI60 = DataNCI60.drop(columns = ['ID'])
+
+    # # replace nan and none values with 0.
+    # DataNCI60.fillna(0, inplace=True)
+
+    # # taking mean expression of all probes per gene.
+    # DataNCI60 = DataNCI60.groupby('Gene Symbol').mean(numeric_only=True)
+
+    # # merging MLR_genes with DataNCI60
+    # DataNCI60 = geneList.merge(DataNCI60, left_on='MLR_genes', right_on='Gene Symbol')
+
+    # NCI_data = DataNCI60.iloc[:,1:]
+    # NCI_norm = np.mean(np.mean(NCI_data.iloc[5:25, :], axis=0), axis=0)
+
+
+
+
+    NCI_data = pd.read_csv('./default_data/NCI_data.txt', header=None, delim_whitespace=True)
     NCI_norm = np.mean(np.mean(NCI_data.iloc[5:25, :], axis=0), axis=0)
+
+
+
+
+    data = geneExpMat.iloc[0:25,1:]
     new_norm = np.mean(np.mean(data.iloc[5:25, :], axis=0), axis=0)
 
     d = new_norm - NCI_norm
@@ -223,10 +237,87 @@ def MLRScore(geneExpMat,input_gseid,annot_table):
     B1 = pd.read_excel('./default_data/RelevantData.xlsx', sheet_name='B1')
     GeneList1 = pd.read_excel('./default_data/RelevantData.xlsx', sheet_name='GeneList1')
     
+    
+
+    YHat1, PredictionsDataGSEEMTGenes8pair1 = MLR3(B1, GeneList1, ['CLDN7', 'VIM/CDH1'], pd.concat([data.iloc[a1,:], data.iloc[a2,:]/data.iloc[a3,:]],axis = 1))
+    
+    YHat1_norm, PredictionsDataGSEEMTGenes8pair1Norm = MLR3(B1, GeneList1, [{'CLDN7'}, {'VIM/CDH1'}], pd.concat([NormData.iloc[a1,:], NormData.iloc[a2,:]/NormData.iloc[a3,:]],axis = 1))
 
 
-    #YhatDataGSEEMTGenes8pair1, PredictionsDataGSEEMTGenes8pair1 = MLR3(B1, GeneList1, [{'CLDN7'}, {'VIM/CDH1'}], pd.concat([data.iloc[a1,:], data.iloc[a2,:]/data.iloc[a3,:]],axis = 1))
-    #YhatDataGSEEMTGenes8pair1Norm, PredictionsDataGSEEMTGenes8pair1Norm = MLR3(B1, GeneList1, [{'CLDN7'}, {'VIM/CDH1'}], [NormData.iloc[a1,:], NormData.iloc[a2,:]/NormData.iloc[a3,:]])
+    ScoreEMT1 = np.full((NUM, 1), np.nan)
+    for j in range(NUM):
+        if YHat1.iloc[j, 0] > YHat1.iloc[j, 2]:
+            ScoreEMT1[j] = YHat1.iloc[j, 1]
+        else:
+            ScoreEMT1[j] = 2.0 - YHat1.iloc[j, 1]
+
+    ScoreEMT1_norm = np.full((NUM, 1), np.nan)
+    for j in range(NUM):
+        if YHat1_norm.iloc[j, 0] > YHat1_norm.iloc[j, 2]:
+            ScoreEMT1_norm[j] = YHat1_norm.iloc[j, 1]
+        else:
+            ScoreEMT1_norm[j] = 2.0 - YHat1_norm.iloc[j, 1]
 
 
-    return "MLR Skipped"
+
+    plt.figure(1)
+    plt.plot(0, 0, '.k')
+    plt.plot(0, 0, '.b')
+    plt.plot(0, 0, '.r')
+    plt.scatter(DataNCI60.iloc[c1, 1:],np.log2((DataNCI60.iloc[c2, 1:].astype(np.float32) / DataNCI60.iloc[c3, 1:].astype(np.float32) + 1)), marker='*', color='black', label='NCI60')
+    plt.scatter(data.iloc[a1, :],np.log2((data.iloc[a2, :].astype(np.float32) / data.iloc[a3, :].astype(np.float32) + 1)), marker='*', color='blue', label='Data')
+    plt.scatter(Norm_data.iloc[a1, :],np.log2((Norm_data.iloc[a2, :].astype(np.float32) / Norm_data.iloc[a3, :].astype(np.float32) + 1)), marker='*', color='red', label='NormData')
+    plt.xlabel('CLDN7 Expression')
+    plt.ylabel('VIM/CDH1 Expression')
+    plt.legend()
+    outfile = './graphs_generated/' + input_gseid 
+    if not os.path.exists(outfile):
+        os.makedirs(outfile)
+    plt.savefig(outfile +'/MLRresult.jpg')
+    plt.close()
+
+    print("Done")
+
+
+    ScoreEMT1_norm = pd.DataFrame(ScoreEMT1_norm)
+    ScoreEMT1_norm.columns = ['MLR']
+    temp = pd.DataFrame(geneExpMat.iloc[:,1:].columns)
+    # temp.pop(0)
+    ScoreEMT1_norm = pd.concat([temp,ScoreEMT1_norm],axis = 1)
+    outfile = './output/' + input_gseid 
+    if not os.path.exists(outfile):
+        os.makedirs(outfile)
+    ScoreEMT1_norm.to_csv(outfile + '/MLR_result.csv', index=False) 
+
+    return ScoreEMT1_norm
+
+
+
+
+
+
+def plot_76GSvsKS(result_76GS, result_KS,result_MLR,input_gseid):
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+
+
+    ax1.scatter(result_76GS['GS76'],result_KS['KS'], marker='*', color='black')
+    ax1.set_xlabel('EMT Score (76GS)')
+    ax1.set_ylabel('EMT Score (KS)')
+
+    ax2.scatter(result_76GS['GS76'],result_MLR['MLR'], marker='*', color='black')
+    ax2.set_xlabel('EMT Score (76GS)')
+    ax2.set_ylabel('EMT Score (MLR)')
+
+    ax3.scatter(result_MLR['MLR'],result_KS['KS'], marker='*', color='black')
+    ax3.set_xlabel('EMT Score (MLR)')
+    ax3.set_ylabel('EMT Score (KS)')
+
+    fig.suptitle('EMT scores coorelation')
+
+    outfile = './graphs_generated/' + input_gseid 
+    if not os.path.exists(outfile):
+        os.makedirs(outfile)
+    plt.savefig(outfile +'/EMT_results_correlation.png')
+    plt.close()
+
